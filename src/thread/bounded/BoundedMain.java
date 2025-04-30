d3be8bc1cca479d9997dcd826d7831dec7451975
ohseunghwan <seunghwan.oh13@hansol.com>
2025-04-30 13:02:58 +0900

    생산자 소비자 문제 1-2

--- src/thread/bounded/BoundedMain.java
+++ src/thread/bounded/BoundedMain.java
@@ -8,6 +8,13 @@ import static util.ThreadUtils.sleep;
 
 public class BoundedMain {
 	public static void main(String[] args) {
+		BoundedQueue queue = new BoundedQueueV1(2);
+		
+		
+		
+		// 생산자, 소비자 실행 순서 선택, 반드시 하나만 선택
+		producerFirst(queue);
+		// consumerFirst(queue);
 		
 	}
 	
