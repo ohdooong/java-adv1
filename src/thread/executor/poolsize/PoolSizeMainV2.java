a2df5f1afccef773d14d5ebf671458c300f7fc77
ohseunghwan <seunghwan.oh13@hansol.com>
2025-07-09 13:17:59 +0900

    Thread Pool 전략 1

---
+++ src/thread/executor/poolsize/PoolSizeMainV2.java
@@ -0,0 +1,32 @@
+package thread.executor.poolsize;
+
+import static thread.executor.ExecutorUtils.printState;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+import thread.executor.RunnableTask;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class PoolSizeMainV2 {
+
+	public static void main(String[] args) {
+		ExecutorService es = Executors.newFixedThreadPool(2);
+		log("pool 생성");
+		printState(es);
+		
+		for (int i = 1; i <= 6; i++) {
+			String taskName = "task-" + i;
+			es.execute(new RunnableTask(taskName));
+			printState(es);
+		}
+	}
+
+}
